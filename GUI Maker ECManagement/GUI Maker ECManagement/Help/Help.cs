b5aa564505bb1073188a571c660c54d9fcb02f47
shinichikudo9x <warcraft.mc@gmail.com>
2016-11-29 23:55:34 +0700

    Help

---
+++ GUI Maker ECManagement/GUI Maker ECManagement/Help/Help.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Maker_ECManagement.Help
+{
+    class Help
+    {
+        public static string EcManagementForm = "Ec Management";
+    }
+}
